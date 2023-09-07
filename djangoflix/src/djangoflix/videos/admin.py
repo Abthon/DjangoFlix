9e2a2a713c605cca0cabad019756049edb8ad798
Abthon <walelignabenezer@gmail.com>
2023-09-06 23:19:10 -0700

    Update the video model and the admin page.

--- djangoflix/src/djangoflix/videos/admin.py
+++ djangoflix/src/djangoflix/videos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Video
 
+
+class VideoManager(admin.ModelAdmin):
+    list_display = ["id","title","is_published","published_date"]
+    list_filter = ["id","active"]
+    readonly_fields = ["id","timestamp","updated"]
+    
 # Register your models here.
+admin.site.register(Video,VideoManager)
--- djangoflix/src/djangoflix/videos/models.py
+++ djangoflix/src/djangoflix/videos/models.py
@@ -1,8 +1,49 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify 
 
-# Create your models here.
 class Video(models.Model):
-    title = models.CharField()
+    class VideoStateOptions(models.TextChoices):
+        DRAFT = 'DR', 'Draft'
+        PUBLISH = 'PU', 'Publish'
+        
+    title = models.CharField(max_length=300)
     description = models.TextField()
-    slug = models.SlugField()
-    video_id = models.CharField()
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=300,null=True,blank=True,default="abc")
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(choices=VideoStateOptions.choices, max_length=200, default=VideoStateOptions.DRAFT)
+    published_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False,null=True,blank=True)
+    
+    
+    def save(self,*args, **kwargs):
+        if(self.state == Video.VideoStateOptions.PUBLISH and self.published_timestamp is None):
+            self.published_timestamp = timezone.now()
+        elif(self.state == Video.VideoStateOptions.DRAFT and self.published_timestamp is not None):
+            self.published_timestamp = None
+        if self.slug is None:
+            self.slug = slugify(self.title) 
+        
+        super().save(*args, **kwargs)
+    
+    @property
+    def is_published(self):
+        return self.active
+    
+    @property
+    def published_date(self):
+        return self.published_timestamp
+    
+    def __self__(self):
+        return self.title
+    
+
+class VideoQuerySet(models.QuerySet):
+    pass
+    
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+    
